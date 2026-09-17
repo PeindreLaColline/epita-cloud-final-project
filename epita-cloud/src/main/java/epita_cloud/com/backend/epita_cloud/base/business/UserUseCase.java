fbda65b08da990f08a46933aa31c1639fd6d9434
Peindre La Colline <PeindreLaColline@gmail.com>
2026-09-17 09:53:40 +0200

    Delete custom login logic

--- epita-cloud/src/main/java/epita_cloud/com/backend/epita_cloud/base/business/UserUseCase.java
+++
@@ -1,8 +0,0 @@
-package epita_cloud.com.backend.epita_cloud.base.business;
-
-import epita_cloud.com.backend.epita_cloud.base.business.dto.LoginReq;
-import epita_cloud.com.backend.epita_cloud.base.business.dto.LoginResp;
-
-public interface UserUseCase {
-    LoginResp login(LoginReq loginReq);
-}
--- epita-cloud/src/main/java/epita_cloud/com/backend/epita_cloud/base/business/dto/LoginResp.java
+++
@@ -1,9 +0,0 @@
-package epita_cloud.com.backend.epita_cloud.base.business.dto;
-
-public record LoginResp(
-        String userId,
-        String username,
-        String token
-){
-
-}
