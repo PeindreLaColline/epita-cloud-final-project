72e9e203335328c693e5e8871e28e870f1cdfe09
Peindre La Colline <PeindreLaColline@gmail.com>
2026-09-18 16:53:00 +0200

    Updated frontend

--- epita-cloud/src/main/java/epita_cloud/com/backend/epita_cloud/config/SecurityConfig.java
+++ epita-cloud/src/main/java/epita_cloud/com/backend/epita_cloud/config/SecurityConfig.java
@@ -40,7 +40,9 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
                                 "/v3/api-docs.yaml",
-                                "/health"
+                                "/health",
+                                "/",
+                                "/index.html"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
